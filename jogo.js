2ab22693b348540e268d39e3e7abd7ee85c543c1
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-09 18:15:21 -0300

    Criando a função para inverter a imagem

--- jogo.js
+++ jogo.js
@@ -22,7 +22,7 @@ function posicaoRandomica() {
 
     var mosquito = document.createElement('img');
     mosquito.src = 'imagens/mosca.png';
-    mosquito.className = tamanhoAleatorio();
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio();
     mosquito.style.left = posicaoX + 'px';
     mosquito.style.top = posicaoY + 'px';
     mosquito.style.position = 'absolute';
@@ -42,5 +42,16 @@ function tamanhoAleatorio() {
     
         case 2:
             return 'mosquito3';
+    }
+}
+
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2);
+    switch (classe) {
+        case 0:
+            return 'ladoA';
+        
+        case 1:
+            return 'ladoB';
     }
 }
