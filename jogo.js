9ef3c1d0453f50a4d0be1712497fe475eee85d9a
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-11 14:35:36 -0300

    Criando a lógica para controlar os pontos de vida

--- jogo.js
+++ jogo.js
@@ -1,5 +1,6 @@
 var altura = 0;
 var largura = 0;
+var vidas = 1;
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight;
@@ -14,6 +15,12 @@ function posicaoRandomica() {
     // removendo o mosquito anterior caso exista
     if (document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove();
+        if (vidas > 3) {
+            alert('Game Over!');
+        } else {
+            document.getElementById('v' + vidas).src = "./imagens/coracao_vazio.png";
+            vidas++;
+        }
     }
 
     var posicaoX = Math.floor(Math.random() * largura) - 90;
@@ -33,6 +40,9 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px';
     mosquito.style.position = 'absolute';
     mosquito.id = 'mosquito';
+    mosquito.onclick = function() {
+        this.remove()
+    }
 
     document.body.appendChild(mosquito);
     tamanhoAleatorio();
